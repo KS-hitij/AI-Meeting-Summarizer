9f1a61d1801d97ae494ee75abc8430d57005dd00
KS-hitij <mamgainkshitij@gmail.com>
2026-07-16 13:54:29 +0530

    Initialized sql database

---
+++ db/project.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from base import Base
+
+class Project(Base):
+    project_id: Mapped[String] = mapped_column(String, primary_key=True)
+    project_name: Mapped[String] = mapped_column(String, nullable=False)
+    created_at: Mapped[String] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[String] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
