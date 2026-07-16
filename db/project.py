8e2a4fa7ff4bf8971750d9f3f67f07b8ed0b0d11
KS-hitij <mamgainkshitij@gmail.com>
2026-07-16 14:54:25 +0530

    Changed data types of table

--- db/project.py
+++ db/project.py
@@ -5,7 +5,7 @@ from base import Base
 
 class Project(Base):
     __tablename__ = "projects"
-    project_id: Mapped[String] = mapped_column(String, primary_key=True)
-    project_name: Mapped[String] = mapped_column(String, nullable=False)
-    created_at: Mapped[String] = mapped_column(DateTime, default=datetime.utcnow)
-    updated_at: Mapped[String] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    project_id: Mapped[str] = mapped_column(String, primary_key=True)
+    project_name: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
