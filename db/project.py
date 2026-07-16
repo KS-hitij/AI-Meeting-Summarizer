58a6bc69d84e7fe44082c36563bce2119ef8a7c8
KS-hitij <mamgainkshitij@gmail.com>
2026-07-16 14:56:47 +0530

    Import datetime module

--- db/project.py
+++ db/project.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 from base import Base
 
@@ -7,5 +8,5 @@ class Project(Base):
     __tablename__ = "projects"
     project_id: Mapped[str] = mapped_column(String, primary_key=True)
     project_name: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
