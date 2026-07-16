ee1f1e106c8c3246dd7664316eb3dd3c3f0b2848
KS-hitij <mamgainkshitij@gmail.com>
2026-07-16 14:23:18 +0530

    Added table name

--- db/project.py
+++ db/project.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from base import Base
 
 class Project(Base):
+    __tablename__ = "projects"
     project_id: Mapped[String] = mapped_column(String, primary_key=True)
     project_name: Mapped[String] = mapped_column(String, nullable=False)
     created_at: Mapped[String] = mapped_column(DateTime, default=datetime.utcnow)
